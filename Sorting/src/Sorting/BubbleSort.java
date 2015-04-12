ce0a288f6f5388f5457d9f101d48a377546afd7c
Fredrick Mandela <fredrick_mandela@brown.edu>
2015-04-12 14:37:12 -0400

    Implemented bubblesort

--- Sorting/src/Sorting/BubbleSort.java
+++ Sorting/src/Sorting/BubbleSort.java
@@ -1,14 +1,23 @@
 package Sorting;
+
 public class BubbleSort {
-	/**
-	 * bubbleSort				sorts an array of integers using bubble sort
-	 * 
-	 * @param unsortedArray		an unsorted array of integers
-	 * @return					the input array, sorted least to greatest
-	 */
-	public static int[] bubbleSort(int[] unsortedArray) {
-		//TODO: S1 implements bubble sort
-		
-		return null;
-	}
+  /**
+   * bubbleSort sorts an array of integers using bubble sort
+   * 
+   * @param unsortedArray an unsorted array of integers
+   * @return the input array, sorted least to greatest
+   */
+  public static int[] bubbleSort(int[] unsortedArray) {
+    for (int i = 0; i < unsortedArray.length - 1; i++) {
+      for (int j = 0; j < unsortedArray.length - i - 1; j++) {
+        if (unsortedArray[j] > unsortedArray[j + 1]) {
+          int swap = unsortedArray[j];
+          unsortedArray[j] = unsortedArray[j + 1];
+          unsortedArray[j + 1] = swap;
+        }
+      }
+    }
+
+    return unsortedArray;
+  }
 }
